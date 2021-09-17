0d7c1d09719400c04a16c2cbd7e8ceaffae6f712
NiculescuAndrei <81990184+NiculescuAndrei@users.noreply.github.com>
2021-09-17 17:20:33 +0300

    Add files via upload

---
+++ HomeworkAssignmentAI/src/main/java/Edge.java
@@ -0,0 +1,19 @@
+
+public class Edge 
+{
+	// weight of the edge
+	public final int cost;
+	// "destination" of the road. 
+	public final Node target;
+	
+	public Edge(Node target, int cost)
+	{
+		this.target = target;
+		this.cost = cost;
+	}
+	
+	public String toString()
+	{
+		return target.name+" "+cost;
+	}
+}
